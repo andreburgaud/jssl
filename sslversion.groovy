1e73dbde76522774cd556cc6a3d23f38d2f9ad7c
Andre Burgaud <andre.burgaud@gmail.com>
2023-02-12 16:21:20 -0600

    minor syntax adjustments like eachWithIndex

--- sslversion.groovy
+++ sslversion.groovy
@@ -6,12 +6,7 @@ import picocli.CommandLine.Help.Ansi
 import java.util.concurrent.Callable
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
-import java.util.concurrent.ForkJoinPool
 import java.util.concurrent.Future
-import java.util.concurrent.LinkedBlockingQueue
-import java.util.concurrent.RejectedExecutionHandler
-import java.util.concurrent.ThreadPoolExecutor
-import java.util.concurrent.TimeUnit
 
 import java.net.Socket
 
@@ -35,15 +30,15 @@ import javax.net.ssl.HostnameVerifier
         $/@|bold,green |___/___/____\_/\___|_| /__/_\___/_||_| |@/$,
         $/@|bold,green                                         |@/$
     ],
-    name = 'sslversion.sh', mixinStandardHelpOptions = true, version = 'sslversion 0.1.0',
-    description = 'Display the SSL/TLS protocol versions enabled on a server')
+    name = 'sslversion.sh', mixinStandardHelpOptions = true, version = 'sslversion 0.2.0',
+    description = 'Display the SSL/TLS protocol versions enabled on a server.')
 @picocli.groovy.PicocliScript
 
-@Parameters(arity="1", description = 'Server(s) or file(s) containing lists of servers')
+@Parameters(arity="1", description = 'Server(s) or file(s) containing lists of servers.')
 @Field String[] servers
 
-@Option(names = [ '-v', '--verbose'], description = 'Display more information.')
-@Field boolean verbose = false
+@Option(names = [ '-d', '--debug'], description = 'Display debug information.')
+@Field boolean debug = false
 
 @Option(names = [ '-w', '--worker'], description = 'Number of workers (default: 1).')
 @Field int workers = 1
@@ -54,7 +49,7 @@ NOT_ENABLED = "not enabled"
 // ================================================================================
 // Logic
 // ================================================================================
-boolean isProtocolEnabled(String server, String protocol, SSLSocketFactory sf, boolean verbose) {
+boolean isProtocolEnabled(String server, String protocol, SSLSocketFactory sf, boolean debug) {
     def (hostname, port) =  server.tokenize(':')
     port = (port ?: "443") as int
 
@@ -67,16 +62,20 @@ boolean isProtocolEnabled(String server, String protocol, SSLSocketFactory sf, b
     SSLSocket sslSocket = (SSLSocket) sf.createSocket(socket, hostname, port, true)
     sslSocket.setEnabledProtocols((String[])["$protocol"])
 
+    boolean res = false
     try {
         sslSocket.startHandshake()
-        return true
+        res = true
     }
     catch(Exception e) {
-        if (verbose) {
-            println("  >>> ${protocol}: ${e}")
+        if (debug) {
+            println(">>> ${protocol}: ${e}")
         }
-        return false
     }
+    finally {
+        sslSocket.close()
+    }
+    return res
 }
 
 def printInfo(String msg) {
@@ -143,7 +142,6 @@ def printProtocolSupport(String protocol, boolean enabled) {
     }
 }
 
-
 def readServersFile(File file) {
     def lines = file.readLines().collect{ it.trim() }
     return lines.findAll { it.size() > 0 && !it.startsWith('#') }
@@ -156,7 +154,7 @@ def printSupportedProtocols() {
 }
 
 SSLSocketFactory initSocketFactory() {
-    // Code needed to discard validation with self signed certificates
+    // No validation
     def nullTrustManager = [
         checkClientTrusted: { chain, authType -> },
         checkServerTrusted: { chain, authType -> },
@@ -166,14 +164,14 @@ SSLSocketFactory initSocketFactory() {
     def nullHostnameVerifier = [
         verify: { hostname, session -> true }
     ]
-    // End of code self-signed certificates
-
+    // End of no validation
+    
     def sc = SSLContext.getInstance('TLSv1.3')
     sc.init(null, [nullTrustManager as X509TrustManager] as TrustManager[], null)
     return sc.getSocketFactory()
 }
 
-if (verbose) {
+if (debug) {
     printSupportedProtocols()
 }
 
@@ -194,85 +192,60 @@ sf = initSocketFactory()
 protocols = ["SSLv2Hello", "SSLv3", "TLSv1", "TLSv1.1", "TLSv1.2", "TLSv1.3"]
 
 def callback(server) {
-    def r = [server: server, error: ""]
+    def res = [server: server, protocols: [], error: ""]
     try {
         for (protocol in protocols) {
-            supported = isProtocolEnabled(server, protocol, sf, verbose)
-            switch (protocol) {
-                case "SSLv2Hello":
-                    r.sslv2 = supported
-                    break
-                case "SSLv3":
-                    r.sslv3 = supported
-                    break
-                case "TLSv1":
-                    r.tlsv1 = supported
-                    break
-                case "TLSv1.1":
-                    r.tlsv11 = supported
-                    break
-                case "TLSv1.2":
-                    r.tlsv12 = supported
-                    break
-                case "TLSv1.3":
-                    r.tlsv13 = supported
-                    break
-            }
+            supported = isProtocolEnabled(server, protocol, sf, debug)
+            res.protocols << supported
         }
     }
     catch(java.net.SocketTimeoutException _) {
-        r.error = "timeout"
+        res.error = "timeout"
     }
     catch(java.net.UnknownHostException _) {
-        r.error = "unknown host"
+        res.error = "unknown host"
     }
     catch(Exception e) {
-        r.error = "${e}"
+        res.error = "${e}"
     }
-    return r
+    return res
 }
 
 ExecutorService executorService = Executors.newFixedThreadPool(workers)
 
-// RejectedExecutionHandler handler = new ThreadPoolExecutor.DiscardOldestPolicy()
-
-// ExecutorService executorService = new ThreadPoolExecutor(workers, workers,
-//     0L, TimeUnit.MILLISECONDS,
-//     new LinkedBlockingQueue<>(expandedServers.size()),
-//     handler)
-
 List<Future> futures = expandedServers.collect { server ->
-    if (verbose) {
+    if (debug) {
         println(">>> ${server}")
     }
     executorService.submit({ -> callback(server) } as Callable)
 }
 
 def spinner = "⣾⣽⣻⢿⡿⣟⣯⣷"
-def results = []
-def i = 0
-futures.each {
-    printInfo "${spinner[i%8]}\r"
-    results << it.get()
-    i += 1
+def results = [:]
+futures.eachWithIndex { future, idx ->
+    printInfo "${spinner[idx%8]}\r"
+    result = future.get()
+    results[result.server] = result
 }
 
 print(" \r")
 
 executorService.shutdown()
 
-results.each { r ->
+if (debug) {
+    println(">>> ${results}")
+}
+    
+results.each { k, v ->
     println()
-    printSuccess "${r.server}:"
-    if (r.error.size() > 0) {
-        printError("  Error: ${r.error}")
+    printSuccess "${k}:"
+    if (v.error.size() > 0) {
+        print "  Error".padRight(12)
+        printError("${v.error}")
     }
     else {
-        printProtocolSupport("SSLv2Hello", r.sslv2)
-        printProtocolSupport("SSLv3", r.sslv3)
-        printProtocolSupport("TLSv1", r.tlsv1)
-        printProtocolSupport("TLSv1.1", r.tlsv11)
-        printProtocolSupport("TLSv1.2", r.tlsv12)
-        printProtocolSupport("TLSv1.3", r.tlsv13)
+        protocols.eachWithIndex { enabled, idx ->
+            printProtocolSupport(enabled, v.protocols[idx])
+        }
     }
 }
